f36926074375065bd18e3cfac4d3d5346a035c0e
Roshan Singh <iprogramyourapp@gmail.com>
2025-12-28 14:15:22 +0530

    fix: remove unnecessary key parameter from _App constructor

--- example/main.dart
+++ example/main.dart
@@ -6,7 +6,7 @@ import 'package:lm_camera/lm_camera.dart';
 void main() => runApp(const _App());
 
 class _App extends StatelessWidget {
-  const _App({super.key});
+  const _App();
 
   @override
   Widget build(BuildContext context) {
